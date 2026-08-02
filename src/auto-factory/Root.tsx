3e08294a9dc17f8d693d3b7865eee826fd0522b4
ashina61 <fan61atik@gmail.com>
2026-08-03 01:12:02 +0300

    V2 Remotion motorunu ana kompozisyona bağla

--- src/auto-factory/Root.tsx
+++ src/auto-factory/Root.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Composition} from 'remotion';
-import {AutoShort, AUTO_FACTORY_FRAMES} from './AutoShort';
+import {AutoShortV2, AUTO_FACTORY_V2_FRAMES} from './AutoShortV2';
 
 export const AutoFactoryRoot: React.FC = () => (
   <Composition
     id="NeoSaniyeAuto"
-    component={AutoShort}
-    durationInFrames={AUTO_FACTORY_FRAMES}
+    component={AutoShortV2}
+    durationInFrames={AUTO_FACTORY_V2_FRAMES}
     fps={30}
     width={1080}
     height={1920}
