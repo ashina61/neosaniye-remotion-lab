547d9ed26a1a4f8905303dfe00fb18eb5442220c
ashina61 <fan61atik@gmail.com>
2026-08-02 14:55:00 +0300

    register DB Cooper layered composition

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import {Composition} from 'remotion';
 import {Video} from './Video';
+import {DBCooperShort} from './db-cooper/DBCooperShort';
+import {DB_COOPER_TOTAL_FRAMES} from './db-cooper/data';
 
 export const RemotionRoot: React.FC = () => (
-  <Composition
-    id="NeoSaniyeShort"
-    component={Video}
-    durationInFrames={900}
-    fps={30}
-    width={1080}
-    height={1920}
-  />
+  <>
+    <Composition
+      id="NeoSaniyeShort"
+      component={Video}
+      durationInFrames={900}
+      fps={30}
+      width={1080}
+      height={1920}
+    />
+    <Composition
+      id="DBCooperShort"
+      component={DBCooperShort}
+      durationInFrames={DB_COOPER_TOTAL_FRAMES}
+      fps={30}
+      width={1080}
+      height={1920}
+    />
+  </>
 );
