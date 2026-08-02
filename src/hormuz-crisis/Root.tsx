2b11df17b86638ccf161fc8ff31d1441e0ab8d41
ashina61 <fan61atik@gmail.com>
2026-08-02 20:02:29 +0300

    Hürmüz kompozisyonunu V8 olarak kaydet

--- src/hormuz-crisis/Root.tsx
+++ src/hormuz-crisis/Root.tsx
@@ -5,7 +5,7 @@ import {HORMUZ_TOTAL_FRAMES} from './data';
 
 export const HormuzRoot: React.FC = () => (
   <Composition
-    id="HormuzCrisisV6"
+    id="HormuzCrisisV8"
     component={HormuzCrisisShort}
     durationInFrames={HORMUZ_TOTAL_FRAMES}
     fps={30}
