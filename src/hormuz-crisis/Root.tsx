58a8d20223e9a566313b122399ab0ba46e2cf8d0
ashina61 <fan61atik@gmail.com>
2026-08-02 18:49:35 +0300

    Hürmüz V5 Remotion kompozisyonunu tanımla

---
+++ src/hormuz-crisis/Root.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Composition} from 'remotion';
+import {HormuzCrisisShort} from './HormuzCrisisShort';
+import {HORMUZ_TOTAL_FRAMES} from './data';
+
+export const HormuzRoot: React.FC = () => (
+  <Composition
+    id="HormuzCrisisV5"
+    component={HormuzCrisisShort}
+    durationInFrames={HORMUZ_TOTAL_FRAMES}
+    fps={30}
+    width={1080}
+    height={1920}
+  />
+);
