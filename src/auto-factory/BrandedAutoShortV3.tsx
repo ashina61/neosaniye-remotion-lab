520c1a36b3316817b76ebf611db4f621cae67855
ashina61 <fan61atik@gmail.com>
2026-08-04 00:04:51 +0300

    feat(renderer): overlay topic-grounded semantic drawings

--- src/auto-factory/BrandedAutoShortV3.tsx
+++ src/auto-factory/BrandedAutoShortV3.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {AbsoluteFill} from 'remotion';
 import {AutoShortV3} from './AutoShortV3';
 import {BrandWatermark} from './BrandWatermark';
+import {SemanticSceneOverlay} from './SemanticSceneOverlay';
 
 export const BrandedAutoShortV3: React.FC = () => (
   <AbsoluteFill>
     <AutoShortV3 />
+    <SemanticSceneOverlay />
     <BrandWatermark />
   </AbsoluteFill>
 );
