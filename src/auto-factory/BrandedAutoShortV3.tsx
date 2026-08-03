b82734133a7b577701102d576a19661778e27914
ashina61 <fan61atik@gmail.com>
2026-08-04 01:23:18 +0300

    chore(visuals): enable topic-specific black-hole overlay

--- src/auto-factory/BrandedAutoShortV3.tsx
+++ src/auto-factory/BrandedAutoShortV3.tsx
@@ -3,11 +3,13 @@ import {AbsoluteFill} from 'remotion';
 import {AutoShortV3} from './AutoShortV3';
 import {BrandWatermark} from './BrandWatermark';
 import {SemanticSceneOverlay} from './SemanticSceneOverlay';
+import {BlackHoleSceneOverlay} from './BlackHoleSceneOverlay';
 
 export const BrandedAutoShortV3: React.FC = () => (
   <AbsoluteFill>
     <AutoShortV3 />
     <SemanticSceneOverlay />
+    <BlackHoleSceneOverlay />
     <BrandWatermark />
   </AbsoluteFill>
 );
