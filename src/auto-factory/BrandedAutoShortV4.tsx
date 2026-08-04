369bc2b76c11ab7ceaaa8d8802c95db2b3bb4027
ashina61 <fan61atik@gmail.com>
2026-08-04 14:50:08 +0300

    Use semantic renderer for locked scenes

--- src/auto-factory/BrandedAutoShortV4.tsx
+++ src/auto-factory/BrandedAutoShortV4.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import {AbsoluteFill} from 'remotion';
+import planJson from '../../public/auto-factory/plan.json';
 import {AutoShortV4} from './AutoShortV4';
 import {BrandWatermark} from './BrandWatermark';
+import {SemanticAutoShortV5} from './SemanticAutoShortV5';
+
+const scenes = ((planJson as unknown as {scenes?: Array<{semanticLockRule?: unknown}>}).scenes ?? []);
+const useSemanticRenderer = scenes.length > 0 && scenes.every((scene) => typeof scene.semanticLockRule === 'string');
 
 export const BrandedAutoShortV4: React.FC = () => (
   <AbsoluteFill>
-    <AutoShortV4 />
+    {useSemanticRenderer ? <SemanticAutoShortV5 /> : <AutoShortV4 />}
     <BrandWatermark />
   </AbsoluteFill>
 );
