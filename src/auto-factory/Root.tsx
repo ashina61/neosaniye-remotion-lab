3733e8005aa90c4520ac46c3935d0d464598159e
ashina61 <fan61atik@gmail.com>
2026-08-04 14:50:17 +0300

    Use generated plan duration for semantic renderer

--- src/auto-factory/Root.tsx
+++ src/auto-factory/Root.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Composition} from 'remotion';
-import {AUTO_FACTORY_V4_FRAMES} from './AutoShortV4';
+import planJson from '../../public/auto-factory/plan.json';
 import {BrandedAutoShortV4} from './BrandedAutoShortV4';
+
+const durationInFrames = Math.round(Number((planJson as {duration: number}).duration) * 30);
 
 export const AutoFactoryRoot: React.FC = () => (
   <Composition
     id="NeoSaniyeAuto"
     component={BrandedAutoShortV4}
-    durationInFrames={AUTO_FACTORY_V4_FRAMES}
+    durationInFrames={durationInFrames}
     fps={30}
     width={1080}
     height={1920}
