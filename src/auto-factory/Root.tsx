a0a446a8949db1ae918789f70a786f92d24e33ee
ashina61 <fan61atik@gmail.com>
2026-08-04 07:51:51 +0300

    feat(v4): switch Auto Factory composition to scene grammar renderer

--- src/auto-factory/Root.tsx
+++ src/auto-factory/Root.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Composition} from 'remotion';
-import {AUTO_FACTORY_V3_FRAMES} from './AutoShortV3';
-import {BrandedAutoShortV3} from './BrandedAutoShortV3';
+import {AUTO_FACTORY_V4_FRAMES} from './AutoShortV4';
+import {BrandedAutoShortV4} from './BrandedAutoShortV4';
 
 export const AutoFactoryRoot: React.FC = () => (
   <Composition
     id="NeoSaniyeAuto"
-    component={BrandedAutoShortV3}
-    durationInFrames={AUTO_FACTORY_V3_FRAMES}
+    component={BrandedAutoShortV4}
+    durationInFrames={AUTO_FACTORY_V4_FRAMES}
     fps={30}
     width={1080}
     height={1920}
